ea99ec0c85e38b5d5b5b354e03d0e0905ce6dd11
brenner.silva01 <bsilva@gfbio.org>
2021-07-21 00:20:09 +0200

    partial commit

--- config/urls.py
+++ config/urls.py
@@ -58,7 +58,7 @@ urlpatterns += [
     # ...
     # Route TemplateView to serve Swagger UI template.
     #   * Provide `extra_context` with view name of `SchemaView`.
-    path('swagger-ui/', TemplateView.as_view(
+    path('swagger/', TemplateView.as_view(
         template_name='swagger-ui.html',
         extra_context={'schema_url':'openapi-schema'},
     ), name='swagger-ui'),
