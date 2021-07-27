0f50100e424cd9d4248074bcf2055f460414905d
brenner.silva01 <bsilva@gfbio.org>
2021-07-27 11:03:04 +0200

    remove aws support

--- config/settings/base.py
+++ config/settings/base.py
@@ -228,12 +228,28 @@ EMAIL_BACKEND = env(
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-timeout
 EMAIL_TIMEOUT = 5
 
+HOST_URL_ROOT = env(
+    "HOST_URL_ROOT",
+    default="https://collections.rdc.gfbio.dev/"
+)
+
 # ADMIN
 # ------------------------------------------------------------------------------
 # Django Admin URL.
-ADMIN_URL = "admin/"
+#ADMIN_URL = "admin/"
+ADMIN_URL = env("DJANGO_ADMIN_URL", default="admin/")
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Brenner Silva""", "bsilva@gfbio.org")]
+# ADMINS = [("""Brenner Silva""", "bsilva@gfbio.org")]
+DJANGO_ADMINS = env.list('DJANGO_ADMINS', default=[
+    "Brenner Silva:bsilva@gfbio.org",
+    "Marc Weber:maweber@mpi-bremen.de",
+    "Ivaylo Kostadinov:ikostadi@gfbio.org",
+])
+ADMINS = [
+    ("""{}""".format(x.split(':')[0]), "{}".format(x.split(':')[1]))
+    for x in DJANGO_ADMINS
+]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
@@ -278,10 +294,10 @@ CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-time-limit
 # TODO: set to whatever value is adequate in your circumstances
-CELERY_TASK_TIME_LIMIT = 5 * 60
+CELERY_TASK_TIME_LIMIT = 15 * 60
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-soft-time-limit
 # TODO: set to whatever value is adequate in your circumstances
-CELERY_TASK_SOFT_TIME_LIMIT = 60
+CELERY_TASK_SOFT_TIME_LIMIT = 10 * 60
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#beat-scheduler
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 # django-allauth
@@ -291,8 +307,10 @@ ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
 ACCOUNT_AUTHENTICATION_METHOD = "username"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = True
+
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION = "optional"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = "gfbio_collections.users.adapters.AccountAdapter"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
@@ -318,3 +336,13 @@ REST_FRAMEWORK = {
 CORS_URLS_REGEX = r"^/api/.*$"
 # Your stuff...
 # ------------------------------------------------------------------------------
+
+# REST API Permissions
+# ------------------------------------------------------------------------------
+REST_SAFE_LIST_IPS = [
+    '127.0.0.1',
+    '[::1]',
+    '172.',  # docker local network /8
+    '10.',  # docker swarm network /8
+]
+REST_SAFE_DOMAINS = []
