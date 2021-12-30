80e77beadf697f25f0f6db3d36689e937ca4490a
brenner.silva01 <bsilva@gfbio.org>
2021-12-30 15:50:25 +0000

    48-jwt-implementation

--- config/urls.py
+++ config/urls.py
@@ -11,6 +11,7 @@ from rest_framework import permissions
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView
 )
 urlpatterns = [
                   path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -35,8 +36,9 @@ urlpatterns += [
 ]
 
 urlpatterns += [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='jwt_obtain_token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='jwt_token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='jwt_token_verify'),
 ]
 # for HTML documentation (swagger)
 
