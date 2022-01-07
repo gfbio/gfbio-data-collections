5d04dfda5186987fef72b9b9f7b7beb9f9776d2b
brenner.silva01 <bsilva@gfbio.org>
2022-01-07 18:21:36 +0000

    Develop

--- config/urls.py
+++ config/urls.py
@@ -13,17 +13,17 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView
 )
+
 urlpatterns = [
                   path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-                  path(
-                      "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-                  ),
+                  # path("",include("gfbio_collections.frontend.urls", namespace="frontend")),
+                  path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
                   # Django Admin, use {% url 'admin:index' %}
                   path(settings.ADMIN_URL, admin.site.urls),
                   # User management
                   path("users/", include("gfbio_collections.users.urls", namespace="users")),
                   path("accounts/", include("allauth.urls")),
-                  # Your stuff: custom urls includes go here
+                  # Collection management
                   path("api/", include("gfbio_collections.collection.urls", namespace="api")),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- gfbio_collections/collection/urls.py
+++ gfbio_collections/collection/urls.py
@@ -6,18 +6,25 @@ from gfbio_collections.collection.api.views import (
     root_view,
     collection_view,
     collection_detail_view,
-    )
+    collection_search_view,
+
+)
 
 from django.urls import path
+from .api import views
 
 app_name = "collection"
 
 urlpatterns = [
     path('', view=root_view),
     path('collections/', view=collection_view, name='collections-list'),
+    path('search/', view=collection_search_view, name='collections-search'),
     path('collections/<int:pk>/', view=collection_detail_view, name='collections-detail'),
+    path('collections/<str:username>/', view=collection_view, name='collections-list'),
     path('users/', view=users_list_view, name="users-list"),
     path('users/<str:username>/', view=users_detail_view, name="users-detail"),
-    path('users/me/', view=users_me_view, name='user-me')
+    path('users/me/', view=users_me_view, name='user-me'),
+    # path('collection_list/', views.CollectionListView.as_view(), name='collection'),
+
 ]
 
