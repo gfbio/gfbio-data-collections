102a0cc07f72e91c57b0c9a54b28ce6e0bf8b49d
bsilva@gfbio.org <bsilva@gfbio.org>
2022-02-21 16:25:03 +0100

    hotfix for schema url_pattern

--- gfbio_collections/users/api/serializers.py
+++ gfbio_collections/users/api/serializers.py
@@ -6,16 +6,16 @@ User = get_user_model()
 #class UserSerializer(serializers.HyperlinkedModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     # me = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    # collections = serializers.HyperlinkedRelatedField(many=True, view_name='collections-detail', read_only=True)
+    # collections = serializers.HyperlinkedRelatedField(many=True, view_name='collection-detail', read_only=True)
     #collections = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     # collections = serializers.PrimaryKeyRelatedField(many=True, read_only=True) #todo: queryset=Collection.objects.filter(pk=User.id))
-    # url = serializers.HyperlinkedIdentityField(view_name='collection:collections-list')
+    # url = serializers.HyperlinkedIdentityField(view_name='collection:collection-list')
 
     class Meta:
         model = User
         fields = ["username", "name", "url"] #, "collections"]
 
         extra_kwargs = {
-            "url": {"view_name": "collection:collections-list", "lookup_field": "username"},
-            # "collection": {"view_name": "collection:collections-detail", "many":True, "read_only":True}
+            "url": {"view_name": "collection:collection-list", "lookup_field": "username"},
+            # "collection": {"view_name": "collection:collection-detail", "many":True, "read_only":True}
         }
