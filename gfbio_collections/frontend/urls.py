a14cf97be05a980c1c04292cc4eb404ffd512417
Linus Franz <lfranz@gfbio.org>
2022-06-23 11:25:34 +0200

    59 Removed old files

--- gfbio_collections/frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_name = 'collection_frontend'
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name="pages/home.html"), name='home'),
-]
-
