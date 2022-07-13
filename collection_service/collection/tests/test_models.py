d0a711a940ebb7b4c16b199d926046d2aec90806
Linus Franz <lfranz@gfbio.org>
2022-07-13 09:21:45 +0200

    62-define-and-implement-collection-datamodels     - Changed mostly formatting in test on behalf of lint

--- collection_service/collection/tests/test_models.py
+++ collection_service/collection/tests/test_models.py
@@ -1,4 +1,3 @@
-import pytest
 import re
 from collection_service.collection.models import Collection
 from django.test import TestCase
@@ -7,7 +6,10 @@ from django.test import TestCase
 class TestCollectionModel(TestCase):
 
     def test_valid(self):
-        test_collection = Collection(external_user_id = 17, set = ["abc", "def", "ghi"], origin="gfbio.collections.testData")
+        test_collection = Collection(
+            external_user_id=17, 
+            set=["abc", "def", "ghi"], 
+            origin="gfbio.collections.testData")
         self.assertEqual(17, test_collection.external_user_id)
         self.assertEqual("abc", test_collection.set[0])
         self.assertEqual("gfbio.collections.testData", test_collection.origin)
@@ -15,8 +17,10 @@ class TestCollectionModel(TestCase):
     def test_autoGuid(self):
         test_collection = Collection()
         self.assertIsNotNone(test_collection.id)
-        self.assertIsNotNone(re.fullmatch(r"^collection_[0-9A-Fa-f]{8}(\-[0-9A-Fa-f]{4}){3}\-[0-9A-Fa-f]{12}$", str(test_collection)))
+        guid_regex = r"^[0-9A-Fa-f]{8}(\-[0-9A-Fa-f]{4}){3}\-[0-9A-Fa-f]{12}$"
+        self.assertIsNotNone(re.fullmatch(guid_regex, str(test_collection.id)))
 
     def test_str(self):
         test_collection = Collection()
-        self.assertIsNotNone(re.fullmatch(r"^collection_[0-9A-Fa-f]{8}(\-[0-9A-Fa-f]{4}){3}\-[0-9A-Fa-f]{12}$", str(test_collection)))
+        name_regex = r"^collection_[0-9A-Fa-f]{8}(\-[0-9A-Fa-f]{4}){3}\-[0-9A-Fa-f]{12}$"
+        self.assertIsNotNone(re.fullmatch(name_regex, str(test_collection)))
