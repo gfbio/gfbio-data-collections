18814194a849fdd48fb98d2a864dec1ca046852b
Linus Franz <lfranz@gfbio.org>
2022-07-15 12:35:41 +0200

    63 transformed viewset to mixins+genericApiView

--- collection_service/collection/api/views.py
+++ collection_service/collection/api/views.py
@@ -1,23 +1,29 @@
-from rest_framework import viewsets
-from rest_framework.response import Response
+from rest_framework import generics, mixins
 from collection_service.collection.api.serializers import CollectionSerializer
 from collection_service.collection.models import Collection
 
 
-class CollectionAPIView(viewsets.ModelViewSet):
+class GenericCollectionView(generics.GenericAPIView):
     queryset = Collection.objects.all()
     serializer_class = CollectionSerializer
 
-    def retrieve(self, request, *args, **kwargs):
-        """Returns the collection with the given id."""
-        return super().retrieve(request, *args, **kwargs)
 
-    def get_collections_for_user(self, request, external_user_id):
+class CollectionDetailView(mixins.RetrieveModelMixin, GenericCollectionView):
+    def get(self, request, *args, **kwargs):
+        """Returns the collection with the given id."""
+        return self.retrieve(request, *args, **kwargs)
+
+
+class UserCollectionListView(mixins.ListModelMixin, GenericCollectionView):
+    def get(self, request, *args, **kwargs):
         """
         Returns a list of all collections for the given user/user-id.
         If the user-id is not assigned to any user or there are no collections for this user, the result is empty.
         """
-        collections_of_the_user = self.get_queryset().filter(
-            external_user_id=external_user_id
-        ).values('id', 'created', 'origin', 'set')
-        return Response(collections_of_the_user)
+        return self.list(request, *args, **kwargs)
+
+    def get_queryset(self):
+        external_user_id_from_path=self.kwargs.get('external_user_id')
+        return super().get_queryset().filter(
+            external_user_id=external_user_id_from_path
+        )
--- collection_service/collection/urls.py
+++ collection_service/collection/urls.py
@@ -1,18 +1,10 @@
 from django.urls import path
-from .api.views import CollectionAPIView
+from .api import views
 
 
 app_name = "collection"
 
-collections_detail = CollectionAPIView.as_view({
-    'get': 'retrieve'
-})
-
-collections_for_user = CollectionAPIView.as_view({
-    'get': 'get_collections_for_user'
-})
-
 urlpatterns = [
-    path('<uuid:pk>/', collections_detail, name='collections-detail'),
-    path('users/<external_user_id>/', collections_for_user, name='collections-for-user'),
+    path('<uuid:pk>/', views.CollectionDetailView.as_view(), name='collections-detail'),
+    path('users/<external_user_id>/', views.UserCollectionListView.as_view(), name='collections-for-user'),
 ]
