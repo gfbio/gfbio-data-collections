ea008bb600406b0c8044239de9c929ab59e06080
Linus Franz <lfranz@gfbio.org>
2022-07-25 10:32:47 +0200

    70-set swagger api documentation as main pages - removed the pages, that are no longer needed, removed them from the template

--- collection_service/users/tests/test_swagger.py
+++ collection_service/users/tests/test_swagger.py
@@ -5,13 +5,13 @@ pytestmark = pytest.mark.django_db
 
 
 def test_swagger_accessible_by_admin(admin_client):
-    url = reverse("api-docs")
+    url = reverse("home")
     response = admin_client.get(url)
     assert response.status_code == 200
 
 
-def test_swagger_ui_not_accessible_by_normal_user(client):
-    url = reverse("api-docs")
+def test_swagger_ui_accessible_by_normal_user(client):
+    url = reverse("home")
     response = client.get(url)
     assert response.status_code == 200
 
