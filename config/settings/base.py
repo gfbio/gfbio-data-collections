17c1e8986270389bab598c4e535fd140976bdc8a
Linus Franz <lfranz@gfbio.org>
2022-07-25 11:56:43 +0200

    65-small corrections - removed the server from spectacular-settings, so every environment can communicate just with it's own API - removed the migrate from the .gitlab-ci.yml because it is part of the start.sh now - removed unused import - added a base-url

--- config/settings/base.py
+++ config/settings/base.py
@@ -319,10 +319,7 @@ SPECTACULAR_SETTINGS = {
     "DESCRIPTION": "Documentation of API endpoints of Collection Service",
     "VERSION": "1.0.0",
     "SERVE_PERMISSIONS": ['rest_framework.permissions.AllowAny'],
-    "SERVERS": [
-        {"url": "http://127.0.0.1:8000", "description": "Local Development server"},
-        {"url": "https://collections.rdc.gfbio.dev ", "description": "Production server"},
-    ],
+    'SCHEMA_PATH_PREFIX': "/api/",
     'PREPROCESSING_HOOKS': ["config.settings.base.whitelist_api_endpoints_preprocessing_hook_func"],
 }
 # Your stuff...
