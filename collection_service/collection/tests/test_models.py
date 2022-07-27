3589f032b553709d8cf5d1ea9a0df8c9df8efca3
Linus Franz <lfranz@gfbio.org>
2022-07-27 12:51:57 +0200

    71-added tests

--- collection_service/collection/tests/test_models.py
+++ collection_service/collection/tests/test_models.py
@@ -13,6 +13,9 @@ class TestCollectionModel(TestCase):
         self.assertEqual(17, test_collection.external_user_id)
         self.assertEqual("abc", test_collection.set[0])
         self.assertEqual("gfbio.collections.testData", test_collection.origin)
+        self.assertTrue(test_collection)
+        self.assertIsNone(test_collection.created)
+        self.assertIsNone(test_collection.modified)
 
     def test_autoGuid(self):
         test_collection = Collection()
