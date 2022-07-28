2c6d4576483ae4520ee41b75320e6daa468b1e78
Linus Franz <lfranz@gfbio.org>
2022-07-28 15:54:19 +0200

    71-changed collection-base-model from django_extensions.db.models.TimeStampedModel to model_utils.models.TimeStampedModel

--- collection_service/collection/tests/test_models.py
+++ collection_service/collection/tests/test_models.py
@@ -1,6 +1,7 @@
 import re
 from collection_service.collection.models import Collection
 from django.test import TestCase
+from django.utils import timezone
 
 
 class TestCollectionModel(TestCase):
@@ -14,8 +15,8 @@ class TestCollectionModel(TestCase):
         self.assertEqual("abc", test_collection.set[0])
         self.assertEqual("gfbio.collections.testData", test_collection.origin)
         self.assertTrue(test_collection)
-        self.assertIsNone(test_collection.created)
-        self.assertIsNone(test_collection.modified)
+        self.assertTrue((timezone.now() - test_collection.created).total_seconds() < 4)
+        self.assertTrue((timezone.now() - test_collection.modified).total_seconds() < 4)
 
     def test_autoGuid(self):
         test_collection = Collection()
